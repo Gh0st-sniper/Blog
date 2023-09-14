dda01922b3c67ce456c6bc4253785f42d419c816
Surya Poojary <77871514+Gh0st-sniper@users.noreply.github.com>
2023-09-14 21:23:05 +0530

    created create.blade.php

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+</head>
+<body>
+
+<form action="/posts" method="post">
+
+<label for=""> Title </label>
+<input type="text" name="title" id=""><br>
+<label for=""> Body </label>
+<input type="text" name="body" id=""><br>
+
+<input type="submit" value="Create">
+</form>
+    
+</body>
+</html>
